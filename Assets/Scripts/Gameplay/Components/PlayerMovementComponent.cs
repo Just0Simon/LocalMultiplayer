d2ade9d7d6d09aa1627a000ad53cfce3a7fa94ae
Just0Simon <98878255+Just0Simon@users.noreply.github.com>
2025-06-19 22:40:41 +0300

    New Input System integration.

--- Assets/Scripts/Gameplay/Components/PlayerMovementComponent.cs
+++ Assets/Scripts/Gameplay/Components/PlayerMovementComponent.cs
@@ -1,17 +1,38 @@
 ﻿using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Gameplay.Components
 {
     public class PlayerMovementComponent : NetworkBehaviour
     {
+        private const string MOVE_ACTION_NAME = "Move"; 
+        
         [SerializeField] private float speed = 5f;
+
+        [SerializeField]
+        private PlayerInput _playerInput;
+        [SerializeField]
+        private InputActionAsset _inputActionMap;
+
+        private InputAction _moveAction;
+
+        private void Awake()
+        {
+            if (!IsLocalPlayer)
+            {
+                _playerInput.enabled = false;
+                return;
+            }
+            
+            _moveAction = _playerInput.actions.FindAction(MOVE_ACTION_NAME);
+        }
 
         private void Update()
         {
-            float moveX = Input.GetAxisRaw("Horizontal");
-            float moveZ = Input.GetAxisRaw("Vertical");
-            Vector3 move = new Vector3(moveX, 0, moveZ).normalized * speed * Time.deltaTime;
+            var rawMove = _moveAction.ReadValue<Vector2>();
+            
+            Vector3 move = new Vector3(rawMove.x, 0, rawMove.y).normalized * speed * Time.deltaTime;
             
             if (!IsOwner)
                 return;
@@ -24,7 +45,7 @@ namespace Gameplay.Components
         }
 
         [ServerRpc(RequireOwnership = true)]
-        public void MoveServerRpc(Vector3 translateVector)
+        private void MoveServerRpc(Vector3 translateVector)
         {
             transform.Translate(translateVector);
         }
